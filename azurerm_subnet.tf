7b6fe154b5506e65bcd783eb3c6ec6d5d0fa5e1c
DaShaun <dashaun@redislabs.com>
2021-09-29 19:59:28 -0500

    Fix vnet

--- azurerm_subnet.tf
+++ azurerm_subnet.tf
@@ -11,5 +11,5 @@ resource "azurerm_subnet" "secondary" {
   enforce_private_link_endpoint_network_policies = true
   name                                           = "redisgeek-secondary-sub"
   resource_group_name                            = azurerm_resource_group.resource_group.name
-  virtual_network_name                           = azurerm_virtual_network.primary.name
+  virtual_network_name                           = azurerm_virtual_network.secondary.name
 }
