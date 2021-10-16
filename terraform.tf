7aaa1965ac5510962565d66fca24bf9141224b11
DaShaun <dashaun@redislabs.com>
2021-10-15 21:05:42 -0500

    remove versions add backend

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,14 @@
 terraform {
-  required_version = ">= 1.0.8"
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.2.1"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.76.0"
     }
   }
+  backend "remote" {}
 }
